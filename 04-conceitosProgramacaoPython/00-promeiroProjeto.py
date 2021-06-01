6e33fca8430bf900ec21e87a9706f426822311c0
Dagberto Rigue <riguealive@gmail.com>
2021-06-01 18:55:49 -0300

    Add files via upload

---
+++ 04-conceitosProgramacaoPython/00-promeiroProjeto.py
@@ -0,0 +1,8 @@
+"""
+ Atenção : Python exige que o código seja indentado. É padrão da linguagem para que ela funcione corretamente.
+ Indentação é o padrão utilizado para organizar um grupo de códigos.
+"""
+
+# Autor : Dagberto Rigue
+
+print("Testando projeto")
