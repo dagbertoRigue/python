e8554f2d40a8badf7c4c17a7425b250958165901
Dagberto Rigue <riguealive@gmail.com>
2021-06-01 19:00:26 -0300

    Add files via upload

--- 04-conceitosProgramacaoPython/03-manipulacao-de-dados.py
+++ 04-conceitosProgramacaoPython/03-manipulacao-de-dados.py
@@ -9,17 +9,15 @@ num_int = 5
 num_dec = 7.2
 valor_str = "qualquer texto"
 
-# concatenando dados de tipos diferentes
+# concatenando dados de tipos diferentes ou casting
 print("O valor é :", num_int)
-print("O valor é : %i" %num_int)
-print("Concatenando strings : " + str(num_int))
+print("O valor é : %i" % num_int)
+print("Concatenando strings : " + str(num_int)) # fazendo cast da variável inteira para string, para concatenar dados de mesmo tipo
 
 print("O valor é : ", num_dec)
-print("O valor é : %.10f" %num_dec)
+print("O valor é : %.10f" % num_dec)
+print("Concatenando strings : " + str(num_dec)) # fazendo cast da variável decimal para string, para concatenar dados de mesmo tipo
+
 print("Concatenando strings : ", valor_str)
-print("Concatenando strings : %s" %valor_str)
-
-# fazendo casting da variável inteira para string, para concatenar dados de mesmo tipo
-
-print("Concatenando strings : " + str(num_dec))
-print("Concatenando strings : " + str(valor_str))
+print("Concatenando strings : %s" % valor_str)
+print("Concatenando strings : " + valor_str)
