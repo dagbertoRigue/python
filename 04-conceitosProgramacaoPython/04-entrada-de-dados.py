6f28ac5e40a9cdcf11a32fb41ad0ec21f2ea258b
Dagberto Rigue <riguealive@gmail.com>
2021-06-01 19:08:07 -0300

    Add files via upload

---
+++ 04-conceitosProgramacaoPython/04-entrada-de-dados.py
@@ -0,0 +1,10 @@
+"""
+ Entrada de dados : é o meio pelo qual, as informações são enviadas para a aplicação
+"""
+
+# Autor : Dagberto Rigue
+
+login = input("Login :")
+senha = input("Senha :")
+
+print("Login digitado : %s, e a senha digitada : %s" %(login, senha))
