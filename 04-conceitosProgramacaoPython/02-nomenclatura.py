fc59257987a88b0f24a369d68e026e57b46aad95
Dagberto Rigue <riguealive@gmail.com>
2021-06-01 18:56:57 -0300

    Add files via upload

---
+++ 04-conceitosProgramacaoPython/02-nomenclatura.py
@@ -0,0 +1,34 @@
+"""
+ Python segue uma nomenclatura para declarar seus caracteres, classes e outros
+
+ Variáveis :
+    Iniciar com letra ou underscore
+    Letras minúsculas
+    Pode conter letra, número ou underscore
+    Ex: minha_classe, classe1
+
+ Pacotes | Módulos :
+    Nomes pequenos
+    Letras minúsculas
+    Ex: os, package
+
+ Classes :
+    Iniciar com letras maiúsculas
+    Nomes compostos inciam com letras maiúsculas
+    Ex: LetraMaiuscula, NomeDaClasse
+
+ Funções
+    Deve utilizar letras minúsculas
+    Nomes compostos unidos por underscore
+    Ex: utilizar_underscore(), enviar_email()
+
+ Parâmetro de Funções (como variáveis)
+    Letras minúsculas
+    Nome compostos unidos por underscore
+    Ex: enviar(nome_do_aquivo), receber(self)
+
+ Constantes :
+    deve utilizar letras maiúsculas
+    Nomes compostos unidos por underscore
+    Ex: PI, VALOR_MAXIMO
+"""
