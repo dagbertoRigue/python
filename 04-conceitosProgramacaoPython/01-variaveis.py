b04e8edf2794dc6367a3eb4846bb0510ad7e571a
Dagberto Rigue <riguealive@gmail.com>
2021-06-01 18:56:11 -0300

    Add files via upload

---
+++ 04-conceitosProgramacaoPython/01-variaveis.py
@@ -0,0 +1,18 @@
+"""
+ Variável : é um local de memória reservado que se utiliza para guardar alguma informação (um valor).
+ Valor é um dado, uma informação que, de acordo com sua característica, possui seu determinado tipo.
+ Além do valor, uma variável é composta por um tipo e um nome.
+ O tipo da variável em Python é declarada implicitamente, ou seja, diferente das demais linguagens,
+ não é preciso declará-la, como em java, por exemplo : int a = 10;
+"""
+
+# Autor : Dagberto Rigue
+
+a = 10
+print(a)
+
+b = 5
+print(a + b)
+
+# Para saber qual é o tipo da variável, utilizar 'type(nome_da_variável)'
+print(type(a))
